5aff4878327bfc18ac53252ee14e37b605371aa6
Kevin Nazario <kevin.l.nazario@gmail.com>
2024-03-07 13:51:03 -0800

    Update script file

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,20 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Read in data using D3
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
-
-// Fetch the JSON data and console log it
+// Fetch the JSON data
 d3.json(url).then(function(data) {
 
+  // Can send names or data.names (data already had a list of names/subject ids for us)
+  let names = data.samples.map(name => name.id);
+  
   //Call createDropDown function to creater interactive dropdown menu by passing list of subject ID numbers. 
   createDropDown(data.names);
+  
+  //DELETE WHEN DONE ******************************8
   console.log(data.samples[1]);
+  
+  //call init to initialize all charts with sample data (element 0, id: 940)
   init(data);
+
 });
 
 // Function will append dropdown options for each name in sample dataset and append value and text
@@ -30,16 +34,14 @@ function init(data){
 }
 
 function updateBar(person){
-  let labels = [];
-  let top10_ids = person.otu_ids.slice(0,10); 
-  for (i = 0; i < top10_ids.length; i++){
-    labels.push(`OTU ${top10_ids[i]}  `);
-  }
+  // slice first 10 (top 10) OTU ids and map text 'OTU' in front of each ID 
+  let top10_ids = person.otu_ids.slice(0,10).map(id => `OTU ${id}`);
 
   // Trace1 for horizontal bar chart
+  // use .reverse for each element in order to get horizontal bar to show in descending order (top to bottom)
   let trace1 = {
     x: person.sample_values.slice(0,10).reverse(),
-    y: labels.reverse(),
+    y: top10_ids.reverse(),
     text: person.otu_labels.slice(0,10).reverse(),
     type: "bar",
     orientation: "h"
@@ -65,17 +67,59 @@ function updateBubble(person){
     mode: 'markers',
     marker: {
       color: person.otu_ids,
-      //colorscale: 'Jet',
+      colorscale:'Earth',
       size: person.sample_values
     }
   };
   
   let data = [trace2];
   
+  var annotations = [
+    {
+      text: 'Color Scale:',
+      y: 1.1,
+      yref: 'paper',  
+      align: 'left',
+      showarrow: false,
+    }
+]
+
+var updatemenus=[
+  {
+      buttons: [
+          {
+              args: ['marker.colorscale', 'Earth'],
+              label: 'Earth',
+              method: 'restyle'
+          },
+          {
+            args: ['marker.colorscale', 'Viridis'],
+            label: 'Viridis',
+            method: 'restyle'
+        },
+          {
+              args: ['marker.colorscale', 'Jet'],
+              label:'Jet',
+              method:'restyle'
+          }
+      ],
+      direction: 'left',
+      pad: {'r': 0, 't': 10},
+      showactive: true,
+      type: 'buttons',
+      x: 0.18,
+      xanchor: 'left',
+      y: 1.15,
+      yanchor: 'top'
+  }
+]
+
   let layout = {
     showlegend: false,
     height: 500,
     width: 800,
+    updatemenus: updatemenus,
+    annotations:annotations
   };
   
   Plotly.newPlot('bubble', data, layout);
@@ -88,7 +132,7 @@ function updateDemo(demographics){
   demo_chart = d3.select('.card-header');
   for (i = 0; i < demo_list.length;i++){
     demo_chart.append('p').text(`${demo_list[i][0]}: ${demo_list[i][1]}`)
-  }
+    }
 }
 
 function optionChanged(id){
