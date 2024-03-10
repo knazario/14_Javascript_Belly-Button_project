8c45a8e6c7ab85214421f66b4b6ffe83a3cb35fa
Kevin Nazario <kevin.l.nazario@gmail.com>
2024-03-10 00:58:36 -0800

    Update file structure for deployed site

--- static/js/app.js
+++ static/js/app.js
@@ -148,12 +148,19 @@ function createGauge(demographics){
       gauge: {
         axis: { range: [null, 9] },
         steps: [
-          { range: [0, 1], color: "lightgray" },
-          { range: [1, 2], color: "gray" }
+          { range: [0, 1], color: "lightgray", name: "0-1"},
+          { range: [7, 8], color: "lightgray", name: "0-1"},
+          { range: [2, 3], color: "lightgray", name: "0-1"},
+          { range: [3, 4], color: "lightgray", name: "0-1"},
+          { range: [4, 5], color: "lightgray", name: "0-1"},
+          { range: [5, 6], color: "lightgray", name: "0-1"},
+          { range: [6, 7], color: "lightgray", name: "0-1"},
+          { range: [7, 8], color: "lightgray", name: "0-1"},
+          { range: [8, 9], color: "gray" }
         ]}
     }
   ];
-  let layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+  let layout = {width: 600, height: 500, margin: { t: 0, b: 0 } };
   Plotly.newPlot('gauge', data, layout);
 }
 
